34e4651a29f43bdff106139ba08aff1fa5a1dca4
Bobby Martin and Matt Miller <bobby@>
2018-03-29 15:04:28 -0700

    create conditionals for spec 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 function cryptosquare(string) {
   var userInputArray = string.split("");
-  console.log(userInputArray);
   var userOutput = userInputArray.join("");
-  console.log(userOutput);
+  var twoByTwo = [];
+  var squareRoot = Math.sqrt(string.length);
+  if(squareRoot % Math.round(squareRoot) === 0) {
+    for (var i = 0; i < squareRoot; i++) {
+      twoByTwo.push([]);
+    }
+    for(var j = 0; j < userInputArray.length; j++) {
+      if(j === 0 || j === 2) {
+        twoByTwo[0].push(userInputArray[j]);
+      } else if(j === 1 || j === 3) {
+        twoByTwo[1].push(userInputArray[j]);
+      }
+    }
+  }
+  console.log(twoByTwo);
 }
