cdf9c0f985bb41083b529eb8a830bbf94402e0ba
Bobby Martin and Matt Miller <bobby@>
2018-03-29 16:28:26 -0700

    Add conditions based on squareroot of user entered string

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,27 @@ function cryptosquare(string) {
   var userInputArray = string.split("");
   var userOutput = userInputArray.join("");
   var twoByTwo = [];
+  var counter = 0;
   var squareRoot = Math.sqrt(string.length);
   if(squareRoot % Math.round(squareRoot) === 0) {
     for (var i = 0; i < squareRoot; i++) {
       twoByTwo.push([]);
     }
-    for(var j = 0; j < userInputArray.length; j++) {
-      if(j === 0 || j === 2) {
-        twoByTwo[0].push(userInputArray[j]);
-      } else if(j === 1 || j === 3) {
-        twoByTwo[1].push(userInputArray[j]);
-      }
+  } else {
+    for (var i = 0; i <= squareRoot; i++) {
+      twoByTwo.push([]);
     }
   }
+
+
+    for(var j = 0; j < userInputArray.length; j++) {
+      twoByTwo[counter].push(userInputArray[j]);
+      if (counter === Math.ceil(squareRoot) - 1) {
+        counter = 0;
+      } else {
+        counter += 1;
+      }
+    }
+
   console.log(twoByTwo);
 }
